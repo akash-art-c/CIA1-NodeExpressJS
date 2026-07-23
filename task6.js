939351a65540d75fdc70ed6a25f5520006575d8a
akash-art-c <l.akash122005@gmail.com>
2026-07-23 09:50:11 +0530

    Add patient list and log details to console

---
+++ task6.js
@@ -0,0 +1,10 @@
+let patients = [
+    { name: "Rahul", age: 30 },
+    { name: "Anjali", age: 25 },
+    { name: "Riya", age: 40 },
+    { name: "Karan", age: 60 }
+];
+
+for (let patient of patients) {
+    console.log("Name:", patient.name, "Age:", patient.age);
+}
