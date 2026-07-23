3ae6ced9fdd861c4664b70b7bcb53c07eb39e139
akash-art-c <l.akash122005@gmail.com>
2026-07-23 09:52:08 +0530

    Add patient object with details and console logs

---
+++ task10.js
@@ -0,0 +1,13 @@
+let patient = {
+    id: 101,
+    name: "Rahul",
+    age: 30,
+    disease: "Fever",
+    admitted: true
+};
+
+console.log("ID:", patient.id);
+console.log("Name:", patient.name);
+console.log("Age:", patient.age);
+console.log("Disease:", patient.disease);
+console.log("Admitted:", patient.admitted);
